c146d7850122c90df723ed0d95451eb90e570c46
Avetik <avetik.avetik@gmail.com>
2018-11-21 18:29:54 +0400

    7

--- src/book/objectorienteddesign/recursion/Method.java
+++ src/book/objectorienteddesign/recursion/Method.java
@@ -1,5 +1,9 @@
 package book.objectorienteddesign.recursion;
 
+import book.objectorienteddesign.ADT.stack.ArrayStack;
+import book.objectorienteddesign.ADT.stack.LinkedStack;
+import book.objectorienteddesign.ADT.stack.Stack;
+
 public class Method {
 
     // Code Fragment Summing an array of integers using linear recursion.
@@ -40,6 +44,68 @@ public class Method {
             data[low++] = data[high]; // post-increment of low
             data[high--] = temp; // post-decrement of high
         }
+    }
+
+    // Code Fragment a generic method that reverses the elements in an array with
+    // objects of type E, using a stack declared with the interface stack<E> as its type.
+
+    /**
+     * A generic method for reversing an array.
+     */
+    public static <E> void reverse(E[] a) {
+        Stack<E> buffer = new ArrayStack<>(a.length);
+        for (int i = 0; i < a.length; i++)
+            buffer.push(a[i]);
+        for (int i = 0; i < a.length; i++)
+            a[i] = buffer.pop();
+    }
+
+    // Code Fragment Method for matching delimiters in an arithmetic expression
+
+    /**
+     * Tests if delimiters in the given expression are properly matched.
+     */
+    public static boolean isMatched(String expression) {
+        final String opening = "({["; // opening delimiters
+        final String closing = ")}]"; // respective closing delimiters
+        Stack<Character> buffer = new LinkedStack<>();
+        for (char c : expression.toCharArray()) {
+            if (opening.indexOf(c) != -1) // this is a left delimiter
+                buffer.push(c);
+            else if (closing.indexOf(c) != -1) { // this is a right delimiter
+                if (buffer.isEmpty()) // nothing to match with
+                    return false;
+                if (closing.indexOf(c) != opening.indexOf(buffer.pop()))
+                    return false; // mismatched delimiter
+            }
+        }
+        return buffer.isEmpty(); // were all opening delimiters matched?
+    }
+
+    // Code Fragment Method for testing if an HTML document has matching tags
+
+    /**
+     * Tests if every opening tag has a matching closing tag in HTML string.
+     */
+    public static boolean isHTMLMatched(String html) {
+        Stack<String> buffer = new LinkedStack<>();
+        int j = html.indexOf('<'); // find first ’<’ character (if any)
+        while (j != -1) {
+            int k = html.indexOf('>', j + 1); // find next ’>’ character
+            if (k == -1)
+                return false; // invalid tag
+            String tag = html.substring(j + 1, k); // strip away < >
+            if (!tag.startsWith("/")) // this is an opening tag
+                buffer.push(tag);
+            else { // this is a closing tag
+                if (buffer.isEmpty())
+                    return false; // no tag to match
+                if (!tag.substring(1).equals(buffer.pop()))
+                    return false; // mismatched tag
+            }
+            j = html.indexOf('<', k + 1); // find next ’<’ character (if any)
+        }
+        return buffer.isEmpty(); // were all opening tags matched?
     }
 
     // Code Fragment Computing the power function using trivial recursion.
